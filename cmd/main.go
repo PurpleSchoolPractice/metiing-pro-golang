6a696456f382de4d308a347fb9b6690718db1d0e
AlexandrLitkevich <litkevitch.aleksandr@yandex.ru>
2025-03-09 13:44:12 +0400

    add go mod

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/app"
+	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/logger"
+	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/server"
+)
 
 func main() {
-	fmt.Println("start")
+	logging := logger.NewLogger()
+	application := app.NewApp()
+
+	srv := server.NewServer(logging, application)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	if err := srv.Start(ctx); err != nil {
+		log.Println(err.Error())
+	}
 }
