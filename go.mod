cc44d03a83a3f6ad9944eff3e34bed293fe8725c
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2025-03-22 11:04:55 +0300

    feature - добавлены user_test

--- go.mod
+++ go.mod
@@ -5,10 +5,13 @@ go 1.23.0
 toolchain go1.24.0
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/fatih/color v1.18.0
 	github.com/go-chi/chi/v5 v5.2.1
+	github.com/joho/godotenv v1.5.1
 	github.com/spf13/cobra v1.9.1
 	github.com/spf13/viper v1.20.0
+	go.uber.org/mock v0.5.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
@@ -17,7 +20,14 @@ require (
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
 	github.com/sagikazarmark/locafero v0.8.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -25,8 +35,9 @@ require (
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
-	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 	golang.org/x/text v0.23.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ internal/user/user_test.go
@@ -0,0 +1,170 @@
+package user_test
+
+import (
+	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/user"
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/PurpleSchoolPractice/metiing-pro-golang/pkg/db"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func setupMockDB(t *testing.T) (*gorm.DB, sqlmock.Sqlmock, func()) {
+	sqlDB, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("Not created sqlmock: %v", err)
+	}
+	dialector := postgres.New(postgres.Config{
+		Conn:                 sqlDB,
+		PreferSimpleProtocol: true,
+	})
+	gormDB, err := gorm.Open(dialector, &gorm.Config{})
+	if err != nil {
+		t.Fatalf("Not opened DB: %v", err)
+	}
+	cleanup := func() {
+		sqlDB.Close()
+	}
+	return gormDB, mock, cleanup
+}
+
+func TestCreateUser(t *testing.T) {
+	gormDB, mock, cleanup := setupMockDB(t)
+	defer cleanup()
+	mock.ExpectBegin()
+	mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "users"`)).
+		WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), sqlmock.AnyArg(), "testuser", "password", "email@example.com").
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+
+	mock.ExpectCommit()
+	dbWrapper := &db.Db{DB: gormDB}
+	repo := user.NewUserRepository(dbWrapper)
+
+	newUser := user.NewUser("email@example.com", "password", "testuser")
+	createdUser, err := repo.Create(newUser)
+	if err != nil {
+		t.Errorf("Error creating user: %v", err)
+	}
+	if createdUser.Email != "email@example.com" {
+		t.Errorf("Exepected email email@example.com, got %s", createdUser.Email)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Error verifying expectations: %v", err)
+	}
+}
+
+func TestFindByEmail(t *testing.T) {
+	gormDB, mock, cleanup := setupMockDB(t)
+	defer cleanup()
+
+	fixedTime := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "username", "password", "email"}).
+		AddRow(1, fixedTime, fixedTime, nil, "testuser", "password", "email@example.com")
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "users" WHERE email = $1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT $2`)).
+		WithArgs("email@example.com", 1).
+		WillReturnRows(rows)
+
+	dbWrapper := &db.Db{DB: gormDB}
+	repo := user.NewUserRepository(dbWrapper)
+	foundUser, err := repo.FindByEmail("email@example.com")
+	if err != nil {
+		t.Errorf("Error fetching user: %v", err)
+	}
+	if foundUser.Email != "email@example.com" {
+		t.Errorf("Expected email email@example.com, got %s", foundUser.Email)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Error verifying expectations: %v", err)
+	}
+}
+
+func TestFindAllUsers(t *testing.T) {
+	gormDB, mock, cleanup := setupMockDB(t)
+	defer cleanup()
+
+	// Используем фиксированное время
+	fixedTime := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
+	// Ожидаем SELECT-запрос для получения всех пользователей.
+	rows := sqlmock.NewRows([]string{"id", "created_at", "updated_at", "deleted_at", "username", "password", "email"}).
+		AddRow(1, fixedTime, fixedTime, nil, "testuser", "password", "email@example.com")
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "users" WHERE "users"."deleted_at" IS NULL`)).
+		WillReturnRows(rows)
+
+	dbWrapper := &db.Db{DB: gormDB}
+	repo := user.NewUserRepository(dbWrapper)
+	users, err := repo.FindAllUsers()
+	if err != nil {
+		t.Errorf("Error fetching users: %v", err)
+	}
+	if len(users) != 1 {
+		t.Errorf("Expected 1 user, got %d", len(users))
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Error verifying expectations: %v", err)
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	gormDB, mock, cleanup := setupMockDB(t)
+	defer cleanup()
+	mock.ExpectBegin()
+	mock.ExpectQuery(regexp.QuoteMeta(`UPDATE "users" SET`)).
+		WithArgs(sqlmock.AnyArg(), "newusername", "newpassword", "email@example.com", 1).
+		WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+	mock.ExpectCommit()
+
+	dbWrapper := &db.Db{DB: gormDB}
+	repo := user.NewUserRepository(dbWrapper)
+
+	userToUpdate := &user.User{
+		Model:    gorm.Model{ID: 1},
+		Username: "newusername",
+		Password: "newpassword",
+		Email:    "email@example.com",
+	}
+	updatedUser, err := repo.Update(userToUpdate)
+	if err != nil {
+		t.Errorf("Error updating user: %v", err)
+	}
+	if updatedUser.Username != "newusername" {
+		t.Errorf("expected newusername, %s", updatedUser.Username)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Error verifying expectations: %v", err)
+	}
+}
+
+func TestDeleteUser(t *testing.T) {
+	gormDB, mock, cleanup := setupMockDB(t)
+	defer cleanup()
+	mock.ExpectBegin()
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE "users" SET "deleted_at"=`)).
+		WithArgs(sqlmock.AnyArg(), 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	dbWrapper := &db.Db{DB: gormDB}
+	repo := user.NewUserRepository(dbWrapper)
+	userToDelete := &user.User{
+		Model:    gorm.Model{ID: 1},
+		Username: "testuser",
+		Password: "password",
+		Email:    "email@example.com",
+	}
+	err := repo.Delete(userToDelete)
+	if err != nil {
+		t.Errorf("Error deleting user: %v", err)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Error verifying expectations: %v", err)
+	}
+}
