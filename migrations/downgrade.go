21832f6c6d24a96cc9843c758763856cea3b9a22
Anypeny <rongard88@gmal.com>
2025-06-03 21:15:50 +0700

    ReBiuld apps and delete logs

--- migrations/downgrade.go
+++ migrations/downgrade.go
@@ -1,45 +1,29 @@
 package migrations
 
 import (
-	"os"
-
-	"github.com/PurpleSchoolPractice/metiing-pro-golang/configs"
 	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/event"
 	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/eventParticipant"
-	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/logger"
 	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/secret"
 	"github.com/PurpleSchoolPractice/metiing-pro-golang/internal/user"
-	"github.com/joho/godotenv"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
 // удаляем дефолтные значения
 func Migrate() error {
-	logging := logger.NewLogger(configs.LoadConfig())
-	err := godotenv.Load(".env")
+	database, logging, err := InitDBWithLogger()
 	if err != nil {
 		logging.Error(err.Error())
 		return err
 	}
-	database, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_DSN")), &gorm.Config{})
-	if err != nil {
-		logging.Error(err.Error())
-		return err
-	}
-	logging.Info("База данных запущена")
+
 	id := []int{1, 2}
-	logging.Info("Deleting event participants...")
 	if err := database.Unscoped().Where("id IN (?)", id).Delete(&eventParticipant.EventParticipant{}).Error; err != nil {
 		logging.Error("Failed to delete default eventParticipant")
 		return err
 	}
-	logging.Info("Deleting events...")
 	if err := database.Unscoped().Where("id IN (?)", []int{1, 2}).Delete(&event.Event{}).Error; err != nil {
 		logging.Error("Failed to delete default events")
 		return err
 	}
-	logging.Info("Emails to delete users")
 	users := []string{"test1@test1.ru", "test2@test2.ru"}
 	if err := database.Unscoped().Where("email IN (?)", users).Delete(&user.User{}).Error; err != nil {
 		logging.Error("Failed to delete users with email test1@test1.ru and test2@test2.ru")
