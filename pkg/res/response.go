25a09b689d3aad4e19c4b92831a3fe8784b53a7d
wysiwyg-git <aria-08@mail.ru>
2026-04-16 21:46:15 +0300

    changed panic to return err in pkg/res/response.go

--- pkg/res/response.go
+++ pkg/res/response.go
@@ -5,11 +5,12 @@ import (
 	"net/http"
 )
 
-func JsonResponse(w http.ResponseWriter, data interface{}, statusCode int) {
+func JsonResponse(w http.ResponseWriter, data any, statusCode int) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
